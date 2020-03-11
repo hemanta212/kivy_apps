5335ac24546406ab4553849379fa3cd4779f20fb
hemanta212 <sharmahemanta.212@gmail.com>
2020-03-11 15:46:32 +0545

    Renames to http_requester

---
+++ http_requester/main.py
@@ -0,0 +1,35 @@
+import requests
+import kivy
+kivy.require('1.10.1')
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
+
+
+class BaseWidget(Widget):
+    def perform_http_request(self, method):
+        url = self.input_url.text
+        if url and not url.startswith("https://"):
+            url = "https://" + url
+
+        try:
+            custom_request = requests.Request(method, url=url)
+            response = requests.Session().send(custom_request.prepare())
+            self.output_box.text = response.text
+        except Exception as E:
+            self.output_box.text = "ERROR:: \n" + str(E)
+
+    def printer(self):
+        print(self.input_url.text)
+        print(self.output_box.text)
+
+
+class TestApp(App):
+    def build(self):
+        win = BaseWidget()
+        return win
+
+
+if __name__ == '__main__':
+    TestApp().run()
